466d516d261773eec5ce1b6772d4ddf4379a1305
Milton Rodrigues <miltonrodrigues713@gmail.com>
2021-01-09 12:34:54 -0300

    Mapping Data to Components

---
+++ src/notes.js
@@ -0,0 +1,28 @@
+const notes = [
+  {
+    key: 1,
+    title: "Note1",
+    content:
+      "Ex dolore est anim officia ex in. Amet ipsum aliqua irure enim. Laborum aliquip dolore dolor exercitation. Pariatur aliquip velit eu excepteur deserunt. Elit sint consequat dolore eiusmod."
+  },
+  {
+    key: 2,
+    title: "Note2",
+    content:
+      "Ex laborum consequat veniam in voluptate nostrud in id ea Lorem. Labore est veniam ut id adipisicing. Mollit nisi occaecat consequat non dolor occaecat qui veniam enim dolor. Ipsum amet ex veniam et ullamco.."
+  },
+  {
+    key: 3,
+    title: "Note3",
+    content:
+      "Velit magna veniam pariatur excepteur culpa cillum irure consequat sit ullamco est sunt exercitation ad. Magna consectetur mollit ut occaecat commodo non sit Lorem laboris proident. Velit eu commodo anim magna do velit laboris veniam adipisicing deserunt ut. Consectetur mollit dolore ad deserunt ea. Laboris reprehenderit sint aliqua magna cillum laboris aute aute."
+  },
+  {
+    key: 4,
+    title: "Note4",
+    content:
+      "Pariatur cillum nulla occaecat exercitation cillum cupidatat. Excepteur laboris aliquip sit sit irure veniam occaecat officia est ea aliquip. Officia sit elit sit tempor do pariatur cupidatat minim commodo. Elit eiusmod sunt et aliquip magna reprehenderit consectetur laboris nisi. Cupidatat nulla officia fugiat ex voluptate aliquip ut dolor ad. Ea enim proident et laborum in dolor in laborum non adipisicing."
+  }
+];
+
+export default notes;
